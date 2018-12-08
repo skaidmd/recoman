e0454f1b022229209b8d1032a6bfdadf9156e5aa
skai <skaidmd@gmail.com>
2018-12-08 15:50:22 +0900

    api結果を表示するresultがようやく表示された！！！！

--- cms/forms.py
+++ cms/forms.py
@@ -3,52 +3,52 @@ from django.core.validators import MaxValueValidator, MinValueValidator
 
 
 class select_manga(forms.Form):
-
     username = forms.CharField(max_length=100,
-                             label='ユーザー名',
-                             # widget=forms.TextInput(attrs={'placeholder': 'ユーザー名'}))
+                               label='ユーザー名',
+                               # widget=forms.HiddenInput(),
+                               # initial='skaidmd'
+                               # widget=forms.TextInput(attrs={'placeholder': 'ユーザー名'}))
                                )
 
     manga1 = forms.CharField(max_length=100,
                              label='漫画１',
                              widget=forms.TextInput(attrs={'placeholder': 'シリーズ名'}))
-    evaluat1=forms.IntegerField(
-    # evaluat1=forms.IntegerField(validators=[MinValueValidator(1),MaxValueValidator(5)],
-                                label='漫画評価１',
-                                widget=forms.TextInput(attrs={'placeholder': '1~5'}))
+    evaluat1 = forms.IntegerField(
+        # evaluat1=forms.IntegerField(validators=[MinValueValidator(1),MaxValueValidator(5)],
+        label='漫画評価１',
+        widget=forms.TextInput(attrs={'placeholder': '1~5'}))
 
     manga2 = forms.CharField(max_length=100,
                              label='漫画２',
                              widget=forms.TextInput(attrs={'placeholder': 'シリーズ名'}))
-    evaluat2=forms.IntegerField(
-    # evaluat2=forms.IntegerField(validators=[MinValueValidator(1),MaxValueValidator(5)],
-                                label='漫画評価２',
-                                widget=forms.TextInput(attrs={'placeholder': '1~5'}))
+    evaluat2 = forms.IntegerField(
+        # evaluat2=forms.IntegerField(validators=[MinValueValidator(1),MaxValueValidator(5)],
+        label='漫画評価２',
+        widget=forms.TextInput(attrs={'placeholder': '1~5'}))
 
     manga3 = forms.CharField(max_length=100,
                              label='漫画３',
                              widget=forms.TextInput(attrs={'placeholder': 'シリーズ名'}))
-    evaluat3=forms.IntegerField(
-    # evaluat3=forms.IntegerField(validators=[MinValueValidator(1),MaxValueValidator(5)],
-                                label='漫画評価３',
-                                widget=forms.TextInput(attrs={'placeholder': '1~5'}))
+    evaluat3 = forms.IntegerField(
+        # evaluat3=forms.IntegerField(validators=[MinValueValidator(1),MaxValueValidator(5)],
+        label='漫画評価３',
+        widget=forms.TextInput(attrs={'placeholder': '1~5'}))
 
     manga4 = forms.CharField(max_length=100,
                              label='漫画４',
                              widget=forms.TextInput(attrs={'placeholder': 'シリーズ名'}))
-    evaluat4=forms.IntegerField(
-    # evaluat4=forms.IntegerField(validators=[MinValueValidator(1),MaxValueValidator(5)],
-                                label='漫画評価４',
-                                widget=forms.TextInput(attrs={'placeholder': '1~5'}))
+    evaluat4 = forms.IntegerField(
+        # evaluat4=forms.IntegerField(validators=[MinValueValidator(1),MaxValueValidator(5)],
+        label='漫画評価４',
+        widget=forms.TextInput(attrs={'placeholder': '1~5'}))
 
     manga5 = forms.CharField(max_length=100,
                              label='漫画５',
                              widget=forms.TextInput(attrs={'placeholder': 'シリーズ名'}))
-    evaluat5=forms.IntegerField(
-    # evaluat5=forms.IntegerField(validators=[MinValueValidator(1),MaxValueValidator(5)],
-                                label='漫画評価５',
-                                widget=forms.TextInput(attrs={'placeholder': '1~5'}))
-
+    evaluat5 = forms.IntegerField(
+        # evaluat5=forms.IntegerField(validators=[MinValueValidator(1),MaxValueValidator(5)],
+        label='漫画評価５',
+        widget=forms.TextInput(attrs={'placeholder': '1~5'}))
 
     def clean_evaluat(self):
         evaluat = self.cleaned_data['evaluat1']
